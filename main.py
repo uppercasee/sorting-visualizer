31de6902d42e2d7d8fd3ca28d62ffaebb91fa2a3
uppercasee <70530668+uppercasee@users.noreply.github.com>
2023-02-15 18:01:56 +0545

    Added version number

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Project: SortingVisualizer
 # Author: uppercasee
 # Date: 2023/02/15
+# Version: 1.0
 # Description: This program is a visualizer for sorting algorithms. It uses tkinter to create a GUI that displays the sorting process.
 
 from sorting import SortVisualizer
